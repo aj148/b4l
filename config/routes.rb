2b119b4cfb16977fdbbb3f71527b2faddb8a3e43
Arihant Jain <aj148@duke.edu>
2016-02-24 22:44:39 -0500

    Nav Bar work

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Brackets4life::Application.routes.draw do
   # static pages
   root :to => 'static_pages#index'
   get '/about' => 'static_pages#about'
-
+  get '/tournament' => 'static_pages#tournament'
+  get '/prizes' => 'static_pages#prizes'
+  get '/charities' => 'static_pages#charities'
   # resources
   resources :brackets # :except => [:edit, :update, :new]
   post '/brackets/new' => 'brackets#new', :as => "new_bracket"
