ccdf2f67318746c0de089fb15517fd8e460e8125
Vishnu Menon <me@vishnumenon.com>
2016-02-28 22:30:09 -0500

    silence errors

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,7 +43,7 @@ Brackets4life::Application.configure do
   config.action_mailer.default_url_options = { :host => 'b4l.herokuapp.com' }  
 config.action_mailer.delivery_method = :smtp  
 config.action_mailer.perform_deliveries = true  
-config.action_mailer.raise_delivery_errors = true  
+config.action_mailer.raise_delivery_errors = false  
 config.action_mailer.default :charset => "utf-8"  
 config.action_mailer.smtp_settings = {  
   address: "smtp.gmail.com",
