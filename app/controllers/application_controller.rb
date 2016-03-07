ca61f076031fc3287692dba4f00e15a1aae18e4e
Arihant Jain <aj148@duke.edu>
2016-03-07 12:23:36 -0500

    Index changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
 	$charities = [["CJ Foundation for SIDS", "CJ Foundation for SIDS"], ["Charlie's Kids", "Charlie's Kids"], 
-	["Duke Cancer Patient Support Program", "Duke Cancer Patient Support Program"], ["Red Cross", "Red Cross"]]
+	["Duke Cancer Patient Support Program", "Duke Cancer Patient Support Program"], ["Butler High School DECA Philanthropy", "Butler High School DECA Philanthropy"]]
 	protect_from_forgery
 end
