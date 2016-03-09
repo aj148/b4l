a1853d0644575a20a6df0c355ca2bb2838d011f0
Arihant Jain <aj148@duke.edu>
2016-03-09 16:48:00 -0500

    add hoops 4 hope to charity grid

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
 	$charities = [["CJ Foundation for SIDS", "CJ Foundation for SIDS"], ["Charlie's Kids", "Charlie's Kids"], 
 	["First Candle", "First Candle"], ["Butler High School DECA Philanthropy", "Butler High School DECA Philanthropy"],
 	["Cure Finders for Cystic Fibrosis", "Cure Finders for Cystic Fibrosis"],["Duke Delta Gamma: Service for Sight","Duke Delta Gamma: Service for Sight"],
-	["Cribs for Kids","Cribs for Kids"]]
+	["Cribs for Kids","Cribs for Kids"],["Hoops 4 Hope","Hoops 4 Hope"]]
 	protect_from_forgery
 end
