e0bbd4f0fc4d1a44207f81db7d201d63d174e637
Arihant Jain <aj148@duke.edu>
2016-03-09 01:24:47 -0500

    Update Charity Grid/ Add Cribs for Kids

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
 	$charities = [["CJ Foundation for SIDS", "CJ Foundation for SIDS"], ["Charlie's Kids", "Charlie's Kids"], 
-	["Duke Cancer Patient Support Program", "Duke Cancer Patient Support Program"], ["Butler High School DECA Philanthropy", "Butler High School DECA Philanthropy"]]
+	["First Candle", "First Candle"], ["Butler High School DECA Philanthropy", "Butler High School DECA Philanthropy"],
+	["Cure Finders for Cystic Fibrosis", "Cure Finders for Cystic Fibrosis"],["Duke Delta Gamma: Service for Sight","Duke Delta Gamma: Service for Sight"],
+	["Cribs for Kids","Cribs for Kids"]]
 	protect_from_forgery
 end
