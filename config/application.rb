7b93732494f27ae8b8a0aab5259bfecfaa5988e0
Arihant Jain <aj148@duke.edu>
2016-03-11 00:50:02 -0500

    add google analytics

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,11 @@ end
 
 module Brackets4life
   class Application < Rails::Application
+
+    if Rails.env == "production"
+      config.middleware.use("Rack::GoogleAnalytics", :web_property_id => "UA-74993220-1")
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
