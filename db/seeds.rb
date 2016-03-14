2fd147eb031762a60e7b7000d667d554f886f5a2
Arihant Jain <aj148@duke.edu>
2016-03-13 20:46:44 -0400

    Seeds  Inputted Brackets Index updates

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,67 +6,67 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Team.create :name => 'Florida', :slot => 0
-Team.create :name => 'ALB/MSM', :slot => 1
+Team.create :name => 'Kansas', :slot => 0
+Team.create :name => 'Austin Peaby', :slot => 1
 Team.create :name => 'Colorado', :slot => 2
-Team.create :name => 'Pittsburgh', :slot => 3
-Team.create :name => 'VCU', :slot => 4
-Team.create :name => 'Steph. F. Austin', :slot => 5
-Team.create :name => 'UCLA', :slot => 6
-Team.create :name => 'Tulsa', :slot => 7
-Team.create :name => 'Ohio State', :slot => 8
-Team.create :name => 'Dayton', :slot => 9
-Team.create :name => 'Syracuse', :slot => 10
-Team.create :name => 'Western Mich.', :slot => 11
-Team.create :name => 'New Mexico', :slot => 12
-Team.create :name => 'Stanford', :slot => 13
-Team.create :name => 'Kansas', :slot => 14
-Team.create :name => 'Eastern Kentucky', :slot => 15
-Team.create :name => 'Virgina', :slot => 16
-Team.create :name => 'Coastal Carolina', :slot => 17
-Team.create :name => 'Memphis', :slot => 18
-Team.create :name => 'Geo. Washington', :slot => 19
-Team.create :name => 'Cincinnati', :slot => 20
-Team.create :name => 'Harvard', :slot => 21
-Team.create :name => 'Michigan State', :slot => 22
-Team.create :name => 'Delaware', :slot => 23
-Team.create :name => 'North Carolina', :slot => 24
-Team.create :name => 'Providence', :slot => 25
-Team.create :name => 'Iowa State', :slot => 26
-Team.create :name => 'N.C. Central', :slot => 27
-Team.create :name => 'Connecticut', :slot => 28
-Team.create :name => "Saint Joseph's", :slot => 29
-Team.create :name => 'Villanova', :slot => 30
-Team.create :name => 'Milwaukee', :slot => 31
-Team.create :name => 'Arizona', :slot => 32
-Team.create :name => 'Weber State', :slot => 33
-Team.create :name => 'Gonzaga', :slot => 34
-Team.create :name => 'Oklahoma State', :slot => 35
-Team.create :name => 'Oklahoma', :slot => 36
-Team.create :name => 'North Dakota State', :slot => 37
-Team.create :name => 'San Diego State', :slot => 38
-Team.create :name => 'New Mexico State', :slot => 39
-Team.create :name => 'Baylor', :slot => 40
-Team.create :name => 'Nebraska', :slot => 41
-Team.create :name => 'Creighton', :slot => 42
-Team.create :name => 'La.-Lafayette', :slot => 43
-Team.create :name => 'Oregon', :slot => 44
-Team.create :name => 'BYU', :slot => 45
-Team.create :name => 'Wisconsin', :slot => 46
-Team.create :name => 'American', :slot => 47
-Team.create :name => 'Wichita State', :slot => 48
-Team.create :name => 'CP/TSU', :slot => 49
-Team.create :name => 'Kentucky', :slot => 50
-Team.create :name => 'Kansas State', :slot => 51
-Team.create :name => 'Saint Louis', :slot => 52
-Team.create :name => 'NCST/XA', :slot => 53
-Team.create :name => 'Louisville', :slot => 54
-Team.create :name => 'Manhattan', :slot => 55
-Team.create :name => 'Massachusetts', :slot => 56
-Team.create :name => 'IOW/TEN', :slot => 57
-Team.create :name => 'Duke', :slot => 58
-Team.create :name => 'Mercer', :slot => 59
-Team.create :name => 'Texas', :slot => 60
-Team.create :name => 'Arizona State', :slot => 61
-Team.create :name => 'Michigan', :slot => 62
-Team.create :name => 'Wofford', :slot => 63
+Team.create :name => 'UCONN', :slot => 3
+Team.create :name => 'Maryland', :slot => 4
+Team.create :name => 'South Dakota St.', :slot => 5
+Team.create :name => 'California', :slot => 6
+Team.create :name => 'Hawaii', :slot => 7
+Team.create :name => 'Arizona', :slot => 8
+Team.create :name => 'Vandy/Wich. St.', :slot => 9
+Team.create :name => 'Miami', :slot => 10
+Team.create :name => 'Buffalo', :slot => 11
+Team.create :name => 'Iowa', :slot => 12
+Team.create :name => 'Temple', :slot => 13
+Team.create :name => 'Villanova', :slot => 14
+Team.create :name => 'UNC-Ash.', :slot => 15
+Team.create :name => 'Oregon', :slot => 16
+Team.create :name => 'Holy/South U.', :slot => 17
+Team.create :name => 'St. Joseph', :slot => 18
+Team.create :name => 'Cincinnati', :slot => 19
+Team.create :name => 'Baylor', :slot => 20
+Team.create :name => 'Yale', :slot => 21
+Team.create :name => 'Duke', :slot => 22
+Team.create :name => 'UNC-W', :slot => 23
+Team.create :name => 'Texas', :slot => 24
+Team.create :name => 'UNI', :slot => 25
+Team.create :name => 'Texas A & M', :slot => 26
+Team.create :name => 'Green Bay', :slot => 27
+Team.create :name => 'Oregon St.', :slot => 28
+Team.create :name => 'VCU', :slot => 29
+Team.create :name => 'Oklahoma', :slot => 30
+Team.create :name => 'CSU Bakersfield', :slot => 31
+Team.create :name => 'N. Carolina', :slot => 32
+Team.create :name => 'FGCU/ FDU', :slot => 33
+Team.create :name => 'USC', :slot => 34
+Team.create :name => 'Providence', :slot => 35
+Team.create :name => 'Indiana', :slot => 36
+Team.create :name => 'Chattanooga', :slot => 37
+Team.create :name => 'Kentucky', :slot => 38
+Team.create :name => 'Stony Brook', :slot => 39
+Team.create :name => 'Notre Dame', :slot => 40
+Team.create :name => 'MICH/TULSA', :slot => 41
+Team.create :name => 'W. Virginia', :slot => 42
+Team.create :name => 'SF Austin', :slot => 43
+Team.create :name => 'Wisconsin', :slot => 44
+Team.create :name => 'Pittsburgh', :slot => 45
+Team.create :name => 'Xavier', :slot => 46
+Team.create :name => 'Webert St.', :slot => 47
+Team.create :name => 'Virginia', :slot => 48
+Team.create :name => 'Hampton', :slot => 49
+Team.create :name => 'Texas Tech', :slot => 50
+Team.create :name => 'Butler', :slot => 51
+Team.create :name => 'Purdue', :slot => 52
+Team.create :name => 'Little Rock', :slot => 53
+Team.create :name => 'Iowa St.', :slot => 54
+Team.create :name => 'Iona', :slot => 55
+Team.create :name => 'Seton Hall', :slot => 56
+Team.create :name => 'Gonzaga', :slot => 57
+Team.create :name => 'Utah', :slot => 58
+Team.create :name => 'Fresno St.', :slot => 59
+Team.create :name => 'Dayton', :slot => 60
+Team.create :name => 'Syracuse', :slot => 61
+Team.create :name => 'Michigan St.', :slot => 62
+Team.create :name => 'Middle Tenn.', :slot => 63
